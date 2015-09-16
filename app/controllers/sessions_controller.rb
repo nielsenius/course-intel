0df1bf13c04e17ec70df435c9a1a52bedaa2a6db
Matthew Nielsen <mqnielsen@me.com>
2015-09-16 14:38:00 -0400

    Fixed login error.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by_username(params[:email])
+    user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to home_path, notice: "Logged In"
